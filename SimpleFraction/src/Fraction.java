32d486d113bcb7839ab4987ba7a81251303b5669
uvsq22301396 <146821546+BENSEGHIRnadjib@users.noreply.github.com>
2023-10-04 07:25:42 +0200

    Add files via upload

---
+++ SimpleFraction/src/Fraction.java
@@ -0,0 +1,17 @@
+public class Fraction {
+    private int numerateur;
+    private int denominateur;
+
+    // Constructeur
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+
+
+    // Méthode toString
+    public String toString() {
+        return "Je suis une fraction.";
+    }
+}
---
+++ SimpleFraction/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Fraction fraction = new Fraction(0, 1);
+        Fraction fraction = new Fraction(1, 1)
+        System.out.println(fraction.toString());
+    }
+}
